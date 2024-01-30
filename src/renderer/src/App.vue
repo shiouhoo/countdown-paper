9a2988ac1d637b1fcb3849ee8d049892b5315e19
shiouhoo <shiouhoo@foxmail.com>
2024-01-30 13:19:15 +0800

    feat: 全屏样式完成

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -32,6 +32,35 @@ onMounted(() => {
     });
 });
 
+// 计算倒计时
+const endtime = '2024-2-9 00:00:00';
+const duration = ref<{
+    days: number;
+    hours: number;
+    minutes: number;
+    seconds: number;
+}>({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0
+});
+setInterval(() => {
+    const now = new Date().getTime();
+    const end = new Date(endtime).getTime();
+    const leftTime = end - now;
+    const days = Math.floor(leftTime / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((leftTime / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((leftTime / 1000 / 60) % 60);
+    const seconds = Math.floor((leftTime / 1000) % 60);
+    duration.value = {
+        days,
+        hours,
+        minutes,
+        seconds
+    };
+}, 1000);
+
 </script>
 
 <template>
@@ -42,7 +71,7 @@ onMounted(() => {
     >
         <TitleBar v-show="!isMaxWindow" />
         <div class="flex h-100%">
-            <div v-if="!isMaxWindow" class="flex flex-1 justify-between">
+            <div v-if="!isMaxWindow" class="flex flex-1 justify-between ellipsis-2">
                 <div
                     v-for="(item,index) in waperList"
                     :key="index"
@@ -50,6 +79,21 @@ onMounted(() => {
                     @click="onChooseWaper(item)"
                 >
                     <img class="w-100%" :src="item.pic">
+                </div>
+            </div>
+            <div v-else class="relative w-100% h-100%">
+                <div class="absolute left-5rem top-50% -translate-y-50% text-#fff text-8rem write-vertical-left -tracking-3rem whitespace-nowrap font-[HongLeiXingShuJianTi]">
+                    放假倒计时
+                </div>
+                <div class="absolute right-2rem top-50% -translate-y-50% text-#fff text-8rem write-vertical-right -tracking-3rem  font-[HongLeiXingShuJianTi]">
+                    <div class="whitespace-nowrap">
+                        <span class="write-initial tracking-1rem h-8rem">{{ duration.days }}</span> 天
+                        <span class="write-initial tracking-1rem h-8rem">{{ duration.hours }}</span> 小时
+                    </div>
+                    <div class="whitespace-nowrap">
+                        <span class="write-initial tracking-1rem h-8rem">{{ duration.minutes }}</span> 分钟
+                        <span class="write-initial tracking-1rem h-8rem">{{ duration.seconds }}</span> 秒
+                    </div>
                 </div>
             </div>
         </div>
