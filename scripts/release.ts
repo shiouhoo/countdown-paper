4270e4eb69c9fa794d3c308d45d8e7e4f8c12599
shiouhoo <shiouhoo@foxmail.com>
2024-01-31 21:07:50 +0800

    chore: 修改release配置

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,11 +1,18 @@
 // conventional-changelog -p angular -i CHANGELOG.md -s
-
+import pkg from '../package.json';
 import { exec } from 'child_process';
-
-exec('conventional-changelog --config ./changelog-option.js -i CHANGELOG.md -s', (error) => {
-    console.log('release');
+// tag
+const tag = `v${pkg.version}`;
+exec(`conventional-changelog --config ./changelog-option.js -i CHANGELOG.md -s && git add . && git commit -m "feat: 发布版本${tag}"`, (error) => {
     if (error) {
-        console.error(`执行的错误: ${error}`);
+        console.error(`执行changelog错误: ${error}`);
         return;
     }
+    exec(`git tag -a ${tag} -m "release ${tag}"`, (error) => {
+        if (error) {
+            console.error(`执行tag错误: ${error}`);
+            return;
+        }
+    });
+    console.log('release');
 });
